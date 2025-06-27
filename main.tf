bae3bc24de9fc8289bd9b191b8df83e884661c69
solomonwilsonr <130649347+solomonwilsonr@users.noreply.github.com>
2025-06-27 15:45:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,11 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
+  cidr_block           = "10.20.0.0/16"
+  enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
     Name = "${var.project}-vpc"
@@ -19,10 +21,10 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_subnet" "public" {
-  count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 8, count.index)
-  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet("10.20.0.0/16", 8, count.index + 1)  # 10.20.1.0/24, 10.20.2.0/24
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
   tags = {
     Name = "${var.project}-public-subnet-${count.index + 1}"
@@ -30,9 +32,9 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
-  count = 2
-  vpc_id = aws_vpc.main.id
-  cidr_block = cidrsubnet("10.0.0.0/16", 8, count.index + 2)
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet("10.20.0.0/16", 8, count.index + 101)  # 10.20.101.0/24, 10.20.102.0/24
   availability_zone = element(data.aws_availability_zones.available.names, count.index)
   tags = {
     Name = "${var.project}-private-subnet-${count.index + 1}"
@@ -40,12 +42,14 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_eip" "nat" {
-  vpc = true
+  tags = {
+    Name = "${var.project}-eip"
+  }
 }
 
 resource "aws_nat_gateway" "natgw" {
   allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.public[0].id
+  subnet_id     = aws_subnet.public[0].id
   tags = {
     Name = "${var.project}-natgw"
   }
@@ -63,15 +67,15 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = 2
-  subnet_id = aws_subnet.public[count.index].id
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.natgw.id
   }
   tags = {
@@ -80,9 +84,7 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private" {
-  count = 2
-  subnet_id = aws_subnet.private[count.index].id
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
-
-data "aws_availability_zones" "available" {}
