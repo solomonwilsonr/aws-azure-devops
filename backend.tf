fda3377492e604dc61a3971544bce7bbab936248
solomonwilsonr <130649347+solomonwilsonr@users.noreply.github.com>
2025-06-27 15:22:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket         = "my-terraform-bucket-aws-azure"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
     encrypt        = true
   }
 }
